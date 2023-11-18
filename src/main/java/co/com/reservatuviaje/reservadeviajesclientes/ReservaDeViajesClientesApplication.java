0d0f6bcb89186eabad39086335a40c089ff6aa6c
Isabel Restrepo <mariaarpo8@gmail.com>
2023-11-18 09:14:17 -0500

    Se crea repositorio transaccional

---
+++ src/main/java/co/com/reservatuviaje/reservadeviajesclientes/ReservaDeViajesClientesApplication.java
@@ -0,0 +1,13 @@
+package co.com.reservatuviaje.reservadeviajesclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaDeViajesClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaDeViajesClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/reservatuviaje/reservadeviajesclientes/ReservaDeViajesClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.reservatuviaje.reservadeviajesclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaDeViajesClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
